c20492c85330ff4876dcd8fab441bbdd407e71f4
Ramesh Natarajan <rnatara@gmail.com>
2020-07-24 16:18:43 -0500

    Initial commit

---
+++ MqttNonDurableConsumer.php
@@ -0,0 +1,77 @@
+<?php
+
+use Mosquitto\Client;
+
+define('SOLACE_URL', 'localhost');
+define('USERNAME', 'subuser');
+define('PASSWORD', 'mqtt123');
+define('PORT', 1883);
+define('TOPIC', 'test/php/#');
+define('CLIENT_ID', "MqttNonDurableConsumer-" . getmypid());
+define('NUMBER_OF_MESSAGES', 10);
+define('MQTT_QOS',1);
+define('CLEAN_SESSION', True);
+define('TIMEOUT', 60);;
+$nmsgs = 0;
+
+#--------------------------------------------------------------------
+# callbacks
+#
+function connect($r) {
+	wprint("Connect successful. response code {$r}");
+}
+
+function subscribe() {
+	wprint("Subscribe successful");
+}
+
+function message($message) {
+	global $nmsgs, $client;
+	$nmsgs++;
+	wprint("({$nmsgs}) Got a message on topic :". $message->topic."\n".$message->payload);
+	if ($nmsgs >= NUMBER_OF_MESSAGES) {
+		wprint("Received {$nmsgs} messages. Ending loop");
+		$client->exitLoop();
+	}
+    #ob_flush(); flush();
+}
+
+function disconnect() {
+	wprint("Disconnected cleanly");
+}
+
+
+# print to browser
+function wprint($s) {
+    echo nl2br(date("Y-m-d H:i:s"). " : {$s}\n");
+}
+
+#-------------------------------------------------------------------------
+# Main
+#
+#ob_start();
+ob_implicit_flush(true);
+ob_end_flush();
+wprint("Connecting to Solace broker : ".SOLACE_URL.":".PORT);
+wprint("Client username : ".USERNAME);
+wprint("CLIENT ID : ".CLIENT_ID);
+wprint("TOPIC : ".TOPIC);
+wprint("MQTT QOS : ".MQTT_QOS);
+wprint(CLEAN_SESSION?"CLEAN SESSION: Yes":"CLEAN SESSION: No");
+
+$client = new Mosquitto\Client(CLIENT_ID, CLEAN_SESSION);
+$client->setCredentials(USERNAME, PASSWORD);
+wprint("ClientID :".CLIENT_ID);
+#ob_flush(); flush();
+
+$client->onConnect('connect');
+$client->onDisconnect('disconnect');
+$client->onSubscribe('subscribe');
+$client->onMessage('message');
+$client->connect(SOLACE_URL, PORT, TIMEOUT);
+wprint("Connected!");
+wprint("Subcribing to topic :".TOPIC."\n");
+#ob_flush(); flush();
+$client->subscribe(TOPIC, MQTT_QOS); 
+
+$client->loopForever();
---
+++ MqttTopicPublisher.php
@@ -0,0 +1,62 @@
+<?php
+
+use Mosquitto\Client;
+
+define('SOLACE_URL', 'localhost');
+define('PORT', 1883);
+define('USERNAME', 'pubuser');
+define('PASSWORD', 'mqtt123');
+define('TOPIC', 'test/php/mqtt/101');
+define('CLIENT_ID', "MqttDirectTopicPublisher-" . getmypid());
+define('NUMBER_OF_MESSAGES', 100);
+define('MQTT_QOS',1);
+define('CLEAN_SESSION', True);
+define('TIMEOUT', 60);
+
+function connect($r) {
+	wprint("Connect successful. response code {$r}");
+}
+
+function disconnect() {
+	wprint("Disconnected cleanly");
+}
+
+# print to browser
+function wprint($s) {
+    echo nl2br(date("Y-m-d H:i:s"). " : {$s}\n");
+    #ob_flush();
+    #flush();
+}
+
+#-------------------------------------------------------------------------
+# Main
+#
+ob_implicit_flush(true);
+ob_end_flush();
+#ob_start();
+wprint("Connecting to Solace broker : ".SOLACE_URL.":".PORT);
+wprint("Client username : ".USERNAME);
+wprint("CLIENT ID : ".CLIENT_ID);
+wprint("TOPIC : ".TOPIC);
+wprint("MQTT QOS : ".MQTT_QOS);
+wprint(CLEAN_SESSION?"CLEAN SESSION: Yes":"CLEAN SESSION: No");
+
+$client = new Mosquitto\Client(CLIENT_ID);
+$client->setCredentials(USERNAME, PASSWORD);
+$client->onConnect('connect');
+$client->onDisconnect('disconnect');
+$client->connect(SOLACE_URL, PORT, TIMEOUT);
+wprint("Connected!");
+
+$nmsgs = 0 ;
+wprint("Publishing  messages (Max: ". NUMBER_OF_MESSAGES. " ):\n");
+while ($nmsgs < NUMBER_OF_MESSAGES) {
+    $nmsgs += 1;
+	$message = "Hello World ". $nmsgs;
+	$client->publish(TOPIC, $message, MQTT_QOS, false);
+    wprint("Published message {$nmsgs} to topic:". TOPIC. ": {$message}");
+	$client->loop();
+	sleep(1);
+}
+
+wprint("Thats all folks!");
